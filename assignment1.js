5345f3aeb93faacdab43f564d7781e974ad954ca
Albert Park <albert.park@hotmail.com>
2019-05-21 00:39:10 -0700

    tly updates to js

--- assignment1.js
+++ assignment1.js
@@ -1,3 +1,9 @@
-function newTask() {
-  
+function redirectHome {
+  var url = "assignment1.html";
+  window.open(url, '_top');
+}
+
+function redirectAbout {
+  var url = "About.html";
+  window.open(url, '_top');
 }
